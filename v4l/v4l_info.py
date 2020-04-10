72a9e6969bc26392bc29505c15821e204b88929c
jbp1 <jbp1@duke.edu>
2020-04-10 11:28:43 -0400

    v4l info

---
+++ v4l/v4l_info.py
@@ -0,0 +1,17 @@
+
+import os
+import v4l2capture
+file_names = [x for x in os.listdir("/dev") if x.startswith("video")]
+file_names.sort()
+for file_name in file_names:
+	path = "/dev/" + file_name
+	print path
+	try:
+		video = v4l2capture.Video_device(path)
+		driver, card, bus_info, capabilities = video.get_info()
+		print "    driver:       %s\n    card:         %s" \
+			"\n    bus info:     %s\n    capabilities: %s" % (
+				driver, card, bus_info, ", ".join(capabilities))
+		video.close()
+	except IOError, e:
+		print "    " + str(e)
